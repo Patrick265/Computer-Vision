3a79d20bdba2926d38a38c976798d607d1f845cb
Patrick <Patrick.dejong@protonmail.com>
2019-04-18 12:00:24 +0200

    [Added] Example project and settings

--- ComputerVision/CV-Week1/Main.cpp
+++ ComputerVision/CV-Week1/Main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	cv::Mat img = cv::imread("lena.jpg");
+	cv::namedWindow("image", cv::WINDOW_NORMAL);
+	cv::imshow("image", img);
+	cv::waitKey(0);
+	return 0;
 }
