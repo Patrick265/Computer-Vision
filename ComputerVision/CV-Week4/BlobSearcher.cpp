ea1d2826353149aed42437854ef68317ae683bd8
Unknown <timdebooij9@hotmail.com>
2019-06-06 12:48:31 +0200

    [Added] Start of finger detection

---
+++ ComputerVision/CV-Week4/BlobSearcher.cpp
@@ -0,0 +1,23 @@
+#include "BlobSearcher.h"
+cv::SimpleBlobDetector::Params params;
+
+
+void setDetector() {
+	params.maxThreshold = 255;
+	params.minThreshold = 0;
+
+	params.filterByColor = false;
+	params.filterByArea = false;
+	params.filterByCircularity = false;
+	params.filterByConvexity = false;
+	params.filterByInertia = false;
+}
+
+std::vector<cv::KeyPoint> find(cv::Mat image) {
+	setDetector();
+	cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+	std::vector<cv::KeyPoint> myBlobs;
+	detector->detect(image, myBlobs);
+	std::cout << "Size of blobs" << myBlobs.size() << std::endl;
+	return myBlobs;
+}
